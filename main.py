e541b537b6d2e8e225393c614c9e3db009e3265b
Jesus Guerrero <jesseguerrero1991@gmail.com>
2025-10-07 12:01:37 -0500

    init

--- main.py
+++ main.py
@@ -5,8 +5,6 @@ from flask_cors import CORS
 from dotenv import load_dotenv
 
 load_dotenv()
-ESRI_CLIENT_SECRET = os.environ.get("ESRI_CLIENT_SECRET")
-ESRI_TOKEN = os.environ.get("ESRI_TOKEN")
 GOOGLE_3D_TILES = os.environ.get("GOOGLE_3D_TILES")
 GOOGLE_STREET_VIEW = os.environ.get("GOOGLE_STREET_VIEW")
 HOST = "localhost"
@@ -19,7 +17,7 @@ CORS(app)
 def index():
     global HOST, UTSA_USERNAME, UTSA_PASSWORD, GOOGLE_3D_TILES, GOOGLE_STREET_VIEW
     with open(os.path.join(app.static_folder, 'index.html'), 'r') as file:
-        content = file.read().replace('{{ host }}', HOST).replace('{{ esri client secret }}', ESRI_CLIENT_SECRET).replace('{{ esri token }}', ESRI_TOKEN).replace('{{ google 3d tiles }}', GOOGLE_3D_TILES).replace('{{ google street view }}', GOOGLE_STREET_VIEW)
+        content = file.read().replace('{{ host }}', HOST).replace('{{ google 3d tiles }}', GOOGLE_3D_TILES).replace('{{ google street view }}', GOOGLE_STREET_VIEW)
     return Response(content, mimetype='text/html')
 
 if __name__ == "__main__":
